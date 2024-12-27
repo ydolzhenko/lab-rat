84b1cf22b4624f42ce4c382b99ee3a9b972a97f4
Yury Dolzhenko <yury.dolzhenko@ah.nl>
2024-12-27 12:58:59 +0100

    gradle-release plugin is added

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,31 +18,9 @@ allprojects {
 
 subprojects {
 
-
-
-    publishing {
-        publications {
-            create<MavenPublication>(project.name) {
-                from(components["java"])
-            }
-        }
-
-        repositories {
-            maven {
-                name = "edGithub"
-                url = uri("https://maven.pkg.github.com/ydolzhenko/lab-rat")
-                credentials {
-                    username = System.getenv("PACKAGE_MASTER")
-                    password = System.getenv("PACKAGE_MASTER_PASSWORD")
-                }
-            }
-        }
-    }
-
     tasks.named("afterReleaseBuild") {
         dependsOn("publish")
     }
-
 
 }
 
@@ -55,6 +33,29 @@ configure<ReleaseExtension> {
     with(git) {
         requireBranch.set("master|legacy\\/[a-zA-Z0-9-_]+")
         // to disable branch verification: requireBranch.set(null as String?)
+    }
+
+    tasks.named("afterReleaseBuild") {
+        dependsOn("publish")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>(project.name) {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "edGithub"
+            url = uri("https://maven.pkg.github.com/ydolzhenko/lab-rat")
+            credentials {
+                username = System.getenv("PACKAGE_MASTER")
+                password = System.getenv("PACKAGE_MASTER_PASSWORD")
+            }
+        }
     }
 }
 
